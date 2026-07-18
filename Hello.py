f7776ac946b63624953634439bd6abbd981e924e
Adan Masood <adanmasood76@gmail.com>
2026-07-18 15:41:48 +0500

    my first day of python

--- Hello.py
+++ Hello.py
@@ -1,2 +1,9 @@
-print("Hello World\n");
-print("My Name is Adan Masood");
+# num1=int(input("Enter first nummbar:"))
+# num2=int(input("Enter second number:"))
+# temp = num1
+# num1 = num2
+# num2 = temp
+# print("After Swapping")
+# print("first number=" , num1)
+# print("Second number=" , num2)
+
